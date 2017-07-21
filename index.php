266e4eb1f0977c3306fbe1abc30fff011d6f1384
Maurício Porfírio <mauriciosporfirio@gmail.com>
2017-07-21 12:59:57 -0300

    Arquivos para teste

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<html lang="pt-br">
+     <head>
+        <title> Teste - File </title>
+        <meta charset="UTF-8" />
+    </head>
+    <body>
+        
+        <form method="post" action="upar.php" enctype="multipart/form-data">
+            Arquivo: <input type="file" name="arquivo" /> &nbsp; &nbsp;
+            <button type="submit"> upar </button>
+        </form>
+        
+    </body>
+</html>
